67ba39cf0e56068fbc444f2dc779519de65fa1dc
Benjamin Rosas <ballPointPenguin@gmx.us>
2017-06-06 20:04:06 -0500

    set HOST to localhost

---
+++ src/components/LogInPage.js
@@ -0,0 +1,5 @@
+import React, { PropTypes } from 'react';
+import TextImput from './common/TextInput';
+import { bindActionnCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as sessionActions from '../actions/sessionActions';
